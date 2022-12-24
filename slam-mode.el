d364d89e5934a171bcf93ad55ef1224c334e2d60
bob16795 <prestosilver.ptp@gmail.com>
2022-12-24 10:19:47 -0500

    fixes

--- slam-mode.el
+++ slam-mode.el
@@ -2,7 +2,6 @@
 
 ;; Author: prestosilver
 ;; Created: 25 Sep 2000
-;; Keywords: SLAM major-mode
 
 ;;; Commentary:
 ;;
@@ -31,20 +30,23 @@
   "Default highlighting expressions for SLAM mode.")
 
 (defvar slam-mode-syntax-table
-  (let ((st (make-syntax-table)))
-    (modify-syntax-entry ?_ "w" st)
-    (modify-syntax-entry ?/ ". 124b" st)
-    (modify-syntax-entry ?* ". 23" st)
-    (modify-syntax-entry ?\n "> b" st)
-    st)
+  (let ((slam-mode-syntax-table (make-syntax-table)))
+    ; This is added so entity names with underscores can be more easily parsed
+          (modify-syntax-entry ?_ "w" slam-mode-syntax-table)
+
+          ; Comment styles are same as C++
+          (modify-syntax-entry ?/ ". 124b" slam-mode-syntax-table)
+          (modify-syntax-entry ?* ". 23" salm-mode-syntax-table)
+          (modify-syntax-entry ?\n "> b" slam-mode-syntax-table)
+          slam-mode-syntax-table)
   "Syntax table for slam-mode.")
 
 (defun slam-mode ()
   "Major mode for editing slam files."
   (interactive)
   (kill-all-local-variables)
-  (set-syntax-table slam-mode-syntax-table)
   (use-local-map slam-mode-map)
+  (set-syntax-table slam-mode-syntax-table)
   (set (make-local-variable 'font-lock-defaults) '(slam-font-lock-keywords))
   (setq major-mode 'slam-mode)
   (setq mode-name "SLAM")
@@ -53,3 +55,4 @@
 (provide 'slam-mode)
 
 ;;; slam-mode.el ends here
+
