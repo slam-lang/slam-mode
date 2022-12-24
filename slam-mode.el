77f0feb42e95e8f320ab6df2741fd8d9a0e021e0
bob16795 <prestosilver.ptp@gmail.com>
2022-12-24 09:59:48 -0500

    update

--- slam-mode.el
+++ slam-mode.el
@@ -1,9 +1,22 @@
+;;; slam-mode-el -- Major mode for editing WPDL files
+
+;; Author: prestosilver
+;; Created: 25 Sep 2000
+;; Keywords: SLAM major-mode
+
+;;; Commentary:
+;;
+;; This mode is an example used in a tutorial about Emacs
+;; mode creation. The tutorial can be found here:
+;; http://two-wugs.net/emacs/mode-tutorial.html
+
+;;; Code:
 (defvar slam-mode-hook nil)
 (defvar slam-mode-map
   (let ((map (make-keymap)))
     (define-key map "\C-j" 'newline-and-indent)
     map)
-  "Slam-mode map")
+  "Slam-mode map.")
 
 ;;;###autoload
 (add-to-list 'auto-mode-alist '("\\.slm\\'" . slam-mode))
@@ -12,10 +25,10 @@
   (list
    '("\\<\\(co\\(?:py\\|vr\\)\\|dump\\|lambda\\|nop\\|\\|s\\(?:im\\|wap\\)\\)\\>" . font-lock-builtin-face)
    '("" . font-lock-variable-name-face))
-  "Minimal highlighting expressions for SLAM mode")
+  "Minimal highlighting expressions for SLAM mode.")
 
 (defvar slam-font-lock-keywords slam-font-lock-keywords-1
-  "Default highlighting expressions for SLAM mode")
+  "Default highlighting expressions for SLAM mode.")
 
 (defvar slam-mode-syntax-table
   (let ((st (make-syntax-table)))
@@ -24,10 +37,10 @@
     (modify-syntax-entry ?* ". 23" st)
     (modify-syntax-entry ?\n "> b" st)
     st)
-  "Syntax table for slam-mode")
+  "Syntax table for slam-mode.")
 
 (defun slam-mode ()
-  "Major mode for editing slam files"
+  "Major mode for editing slam files."
   (interactive)
   (kill-all-local-variables)
   (set-syntax-table slam-mode-syntax-table)
@@ -38,3 +51,4 @@
   (run-hooks 'slam-mode-hook))
 
 (provide 'slam-mode)
+;;; slam-mode.el ends here
