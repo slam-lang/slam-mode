f5fcb42f8e58a95704d12d5a31abca0e136df196
bob16795 <prestosilver.ptp@gmail.com>
2023-01-02 10:07:31 -0500

    update

--- slam-mode.el
+++ slam-mode.el
@@ -22,19 +22,32 @@
 
 (defconst slam-font-lock-keywords-1
   (list
-   '("\\<\\(argv?\\|co\\(?:py\\|vr\\)\\|d\\(?:isc\\|ump\\)\\|lambda\\|nop\\|putc?\\|readc?\\|s\\(?:im\\|wap\\)\\)\\>" . font-lock-builtin-face)
-   '("\\<\\(proc\\)\\>" . font-lock-constant-face)
-   '("\\('\\w*'\\)" . font-lock-variable-name-face)
+   '("\\<\\(sim\\|lambda\\|nop\\|swap\\|dump\\|copy\\|covr\\|readc\\|read\\|putc\\|put\\|disc\\|argv\\|argc\\|envp\\|const\\|quit\\|if\\|proc\\|do\\|end\\|inc\\|enum\\|var\\|gvar\\|prop\\|class\\|cproc\\|lnk\\)\\>" . font-lock-builtin-face)
   )
   "Minimal highlighting expressions for SLAM mode.")
+(defconst slam-font-lock-keywords-2
+  (append slam-font-lock-keywords-1
+    (list
+     '("\\<\\(brk\\|macro\\|ret\\|asm\\|of\\|temp\\|push\\|oper\\|-\\|*\\|,\\|/%\\|+\\|^\\|!\\|!=\\|==\\|<\\|>\\|&&\\|||\\|()\\|sys0\\|sys1\\|sys2\\|sys3\\|sys4\\|sys5\\|sys6\\|[\\|]\\|\\)\\>" . font-lock-builtin-face)
+    )
+  )
+  "Minimal highlighting expressions for SLAM mode.")
+(defvar slam-font-lock-keywords slam-font-lock-keywords-2
+  "Default highlighting expressions for SLAM mode.")
+
+
+;(defconst slam-font-lock-keywords-1
+;  (list
+;   '("\\<\\(argv?\\|co\\(?:py\\|vr\\)\\|d\\(?:isc\\|ump\\)\\|lambda\\|nop\\|putc?\\|readc?\\|s\\(?:im\\|wap\\)\\)\\>" . font-lock-builtin-face)
+;   '("\\<\\(proc\\)\\>" . font-lock-constant-face)
+;   '("\\('\\w*'\\)" . font-lock-variable-name-face)
+;  )
+;  "Minimal highlighting expressions for SLAM mode.")
 
 ; (regexp-opt '("sim" "lambda" "nop" "swap" "dump" "copy"
 ; "covr" "readc" "read" "putc" "put" "disc" "argv" "argc"
 ; "envp" "sys0" "sys1" "sys2" "sys3") t)
 ; ".\\(argv?\\|co\\(?:py\\|vr\\)\\|d\\(?:isc\\|ump\\)\\|lambda\\|nop\\|putc?\\|readc?\\|s\\(?:im\\|wap\\)\\)"
-
-(defvar slam-font-lock-keywords slam-font-lock-keywords-1
-  "Default highlighting expressions for SLAM mode.")
 
 (defvar slam-mode-syntax-table
   (let ((syntax-table (make-syntax-table)))
